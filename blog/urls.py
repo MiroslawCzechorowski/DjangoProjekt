911b90213b8362428882c76c2af929dc1293407f
Miroslaw Czechorowski <miroslawczechorowski@gmail.com>
2016-12-28 21:29:06 +0100

    Basic CSS

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
     url(r'^post/new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-]
+    url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
