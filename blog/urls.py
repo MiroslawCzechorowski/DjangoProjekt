da47f8046cb0ecc8bdee32cd7fcca26db23b0e4f
Miroslaw Czechorowski <miroslawczechorowski@gmail.com>
2017-01-01 18:55:24 +0100

    Added Login/logout

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^post_new/$', views.post_new, name='post_new'),
     url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/(?P<pk>\d+)/remove/$', views.post_remove, name='post_remove'),
+    url(r'^login/$', auth_views.login,{'template_name': 'blog/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page': '/'}, name='logout')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
