b90d0aeae4852a68c10ba7f02f9a3740795f4809
Miroslaw Czechorowski <miroslawczechorowski@gmail.com>
2017-01-27 20:51:47 +0100

    Improved code style

--- Coupons/urls.py
+++ Coupons/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     url(r'', include('events.urls')),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^newsletter/', include('newsletter.urls')),
-    url(r'^comments/', include('django_comments.urls')),
 ]
